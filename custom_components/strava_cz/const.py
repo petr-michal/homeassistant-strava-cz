7a73605d5a4f5b2748dc877994a5761015c4c9d5
Petr Michal <petr@michalowie.cz>
2026-09-07 21:23:52 +0200

    pridat konstanty pro osobni ucet

--- custom_components/strava_cz/const.py
+++ custom_components/strava_cz/const.py
@@ -6,9 +6,18 @@ from homeassistant.const import Platform
 
 DOMAIN = "strava_cz"
 
+ACCOUNT_TYPE_CANTEEN = "canteen"
+ACCOUNT_TYPE_PERSONAL = "personal"
+
+CONF_ACCOUNT_TYPE = "account_type"
+CONF_ACCOUNTS = "accounts"
 CONF_CANTEEN_NUMBER = "canteen_number"
 
 DEFAULT_UPDATE_INTERVAL = timedelta(hours=1)
 API_TIMEOUT = 60.0
+
+PERSONAL_S4_FIELDS = "VERZE,URLWSDL_S-URL,UR_OTVIRAK,IGN_CERT"
+
+LEGACY_CHILD_KEY = "account"
 
 PLATFORMS = [Platform.SENSOR, Platform.SELECT]
